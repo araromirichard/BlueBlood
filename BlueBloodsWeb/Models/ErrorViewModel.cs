44186a6a4436e29e00588686429d78fa838cbfe9
karosaxy <araromirichard@gmail.com>
2023-10-02 18:42:42 +0100

    first commit

---
+++ BlueBloodsWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BlueBloodsWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
